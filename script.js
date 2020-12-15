183de17547633431bd821e081921b75f88ec40ff
anshika gupta <tanyagupta391@gmail.com>
2020-12-15 13:05:47 +0530

    learnt post command

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
- app.get('/', function(req,res){
-    res.send("hello world!")
- })
+var list = []
 app.set('view engine', 'ejs')
- app.get('/anshika', function(req,res) {
-     res.send("this is my nodeday-2.")
- })
- app.get('/axxu', function(req,res) {
-    res.render('anxx.ejs', {
-        axxu: 'anxxica'
-    })
- })
- app.listen(3000, () => {
-     console.log("server is listening at 3000")
- })
+app.use(express.urlencoded({extended: false}))
+app.get('/', (req,res) => {
+    res.render('anxx.ejs')
+})
+
+app.post('/anshika', (req,res) => {
+    var listelement = req.body.LIST 
+    list.push(listelement)
+    res.send(listelement + " added to the list.")
+})
+
+app.get('/checkList', (req,res) => {
+    res.send("HERE IS YOUR LIST " + list)
+})
+
+app.listen(3000, () => {
+    console.log("We're listening to you miss Gupta")
+})
