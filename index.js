65111a601692419de24c75c014c595e365370208
anshika gupta <tanyagupta391@gmail.com>
2020-12-16 16:59:24 +0530

    post learned

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-app.set('view engine' , 'ejs')
-app.use(express.urlencoded({extended:false}))
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended : false}))
 
-mongoose.connect('mongodb://127.0.0.1:27017/').then(() => {
-    console.log("We\'re connected by heart.")
+mongoose.connect('mongodb://127.0.0.1:27017/', {useNewUrlParser: true, useUnifiedTopology:true})
+.then(() => {
+    console.log("MongoDB connected.")
+})
+
+app.get('/', (req,res) => {
+    res.render("anxxica.ejs")
 })
 
 var schema = mongoose.Schema
 
-var peoples = schema({
-    peoplesid : Number,
-    peoplesname : String,
-    peoplesgoal : String
+var shoppingLists = schema({
+    item1 : String,
+    item2 : String,
+    item3 : String
+})
 
- })
- var peoplesModel = mongoose.model('people', peoples)
- 
- app.post('/addPeople', (req,res) => {
-    var id = req.body.id
-    var name = req.body.name
-    var goal = req.body.goal
-    new peoplesModel({peoplesid : id, peoplesname: name, peoplesgoal : goal}).save().then(document => {
-        console.log(document)
-        res.send("We have your data.")
+var shoppingListModel = mongoose.model('shoppingList', shoppingLists)
+app.post('/storeList', (req,res) => {
+    var it1 = req.body.item1 
+    var it2 = req.body.item2 
+    var it3 = req.body.item3
+    new shoppingListModel({item1 : it1, item2 : it2, item3 : it3}).save().then(createDoc => {
+        res.send("WE HAVE YOUR LIST.")
+        console.log("Saved list.")
     })
- })
-app.get('/checkPeople' , (req,res) => {
-    peoplesModel.find()
-    .then(document =>{
-        res.send(document)
+})
+
+app.get('/seelist', (req,res) => {
+    shoppingListModel.find().then(docs => {
+        res.send(docs)
     })
-}) 
-app.get('/', (req,res) => {
-    res.render('anxxica.ejs')
 })
 
 app.listen(3000, () => {
-    console.log("I am listening to your heart.")
+    console.log("We are listening to you.")
 })
